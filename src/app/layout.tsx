310f7d52f94a0f02caf6be0bb993ce37ada9535d
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-21 20:25:42 +0100

    update layout styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { twMerge } from "tailwind-merge";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={twMerge(inter.className, "bg-black text-white antialiased")}>
         {children}
       </body>
     </html>
