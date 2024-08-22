f9138be9d45037072953b1895e02e56c539c03e1
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-22 18:04:33 +0100

    add animations to the testimonials section, add testimonial component

---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image, { StaticImageData } from 'next/image';
+
+interface TestimonialProps {
+    text: string,
+    name: string,
+    title: string,
+    avatarImg: StaticImageData,
+}
+
+export const Testimonial = (testimonial: TestimonialProps) => {
+    return (
+        <div key={testimonial.name} className="border border-white/15 rounded-xl p-6 md:p-10 bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
+            <div className="text-lg md:text-2xl tracking-tight">{testimonial.text}</div>
+            <div className="flex items-center gap-3 mt-5">
+                <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
+                    <Image src={testimonial.avatarImg} alt={`Avatar for ${testimonial.name}`} className="size-11 rounded-lg grayscale" />
+                </div>
+                <div>
+                    <div>{testimonial.name}</div>
+                    <div className="text-white/50 text-sm">{testimonial.title}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { motion } from "framer-motion";
+
+import { Testimonial } from "@/components/Testimonial";
+
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
-import Image from "next/image";
 
 const testimonials = [
   {
@@ -36,23 +40,24 @@ export const Testimonials = () => {
     <div className="container">
       <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Behond Expectations.</h2>
       <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm md:max-w-md mx-auto">Our revolutionary AI SEO tools have transformed our client's strategies.</p>
-      <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-        <div className="flex gap-5">
-          {testimonials.map((testimonial) => (
-            <div key={testimonial.name} className="border border-white/15 rounded-xl p-6 md:p-10 bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
-              <div className="text-lg md:text-2xl tracking-tight">{testimonial.text}</div>
-              <div className="flex items-center gap-3 mt-5">
-                <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
-                  <Image src={testimonial.avatarImg} alt={`Avatar for ${testimonial.name}`} className="size-11 rounded-lg grayscale" />
-                </div>
-                <div>
-                  <div>{testimonial.name}</div>
-                  <div className="text-white/50 text-sm">{testimonial.title}</div>
-                </div>
-              </div>
-            </div>
+      <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <motion.div
+          initial={{
+            translateX: "-50%",
+          }}
+          animate={{
+            translateX: "0",
+          }}
+          transition={{
+            repeat: Infinity,
+            ease: "linear",
+            duration: 45,
+          }}
+          className="flex gap-5 flex-none pr-5">
+          {[...testimonials, ...testimonials].map((testimonial) => (
+            <Testimonial  {...testimonial} />
           ))}
-        </div>
+        </motion.div>
       </div>
     </div>
   </section>;
