fe0b78360c60dbd3f66c55fb08df45ba2c72fd04
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-22 15:17:30 +0100

    add animation to the logo ticker section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { motion } from "framer-motion";
+
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
@@ -12,12 +15,25 @@ export const LogoTicker = () => {
         <div className="flex-1 md:flex-none">
           <h2>Trusted by top innovative teams</h2>
         </div>
-        <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex flex-none gap-14">
-            {[acmeLogo, pulseLogo, echoLogo, celestialLogo, apexLogo, quantumLogo].map((logo) => (
+        <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{
+              translateX: "-50%",
+            }}
+            animate={{
+              translateX: "0"
+            }}
+            transition={{
+              repeat: Infinity,
+              duration: 30,
+              ease: "linear"
+            }}
+            className="flex flex-none gap-14 pr-14"
+          >
+            {[acmeLogo, pulseLogo, echoLogo, celestialLogo, apexLogo, quantumLogo, acmeLogo, pulseLogo, echoLogo, celestialLogo, apexLogo, quantumLogo].map((logo) => (
               <img src={logo.src} className="h-6 w-auto" />
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
